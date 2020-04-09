be25d907992efb469bdd7326d846b23b8ed7a6f0
Squirtle <tienhoang.pham@bc.fi>
2020-04-09 12:30:07 +0300

    add api calls to 2 endpoints

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-const express = require('express')
-const PORT = process.env.PORT || 5000
-
+const express = require('express');
+const PORT = process.env.PORT || 5000;
+const fetch = require('node-fetch');
 const app = express();
+
+function handleJSON(json){
+	if (!json.data) {
+		return;
+	}
+
+	var result = [],
+		counter = json.data.length;
+
+	while(counter--){
+		try {
+			result.push(json.data[counter].images.original.url);
+		} catch {
+			console.log('An error has occurred while processing GIHPY response');
+		}	
+	}
+
+	return result;
+}
+
+function buildUrl(endpoint, params){
+	return endpoint + '?api_key=' + params.apiKey + '&q=' + params.searchTerm;
+}
+
+function fetchGif(apiUrl){
+	return fetch(apiUrl)
+		.then(function(resp){
+			return resp.json();
+		})
+		.then(function(json){			
+			var formattedData = handleJSON(json);
+			return formattedData;
+		})
+		.catch(function(err){
+			res.send('An error has occurred: ' + err);
+		});
+}
 
 app
 	.use(express.static(__dirname + '/public/'))
@@ -12,5 +49,24 @@ app.get('/', function (req, res) {
 });
 
 app.post('/api/gif/:searchTerm', function(req, res){
-	res.send('POST request successful');
+	var gifEndPoint = 'http://api.giphy.com/v1/gifs/search',
+		stickerEndPoint = 'http://api.giphy.com/v1/stickers/search',
+		params = {
+			apiKey: 'fIHqnkqtM7ST2n3JkRogMqokprhiWs7h',
+			searchTerm: req.params.searchTerm
+		},
+		gifEndPointUrl = buildUrl(gifEndPoint, params),
+		stickerEndPointUrl = buildUrl(stickerEndPoint, params),
+		promises = [fetchGif(gifEndPointUrl), fetchGif(stickerEndPointUrl)];
+
+	Promise.all(promises)
+		.then(function(resp){
+			res.send({
+				gifs: resp[0],
+				stickers: resp[1]
+			});
+		})
+		.catch(function(err){
+			res.send('An Error has occured: ' + err);
+		});
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,16 @@
+function renderImg(data){
+    data.forEach(function(imgSrc){
+        var imgEl = document.createElement('img');
+        imgEl.src = imgSrc;
+        document.getElementById('result').appendChild(imgEl);
+    });
+}
+
 document.getElementById('search-btn').addEventListener('click', function(event){
     event.preventDefault();
 
     var searchTerm = document.getElementById('searchTerm').value,
-        apiUrl = '//bch-node-learning.herokuapp.com/api/gif/' + searchTerm,
+        apiUrl = '/api/gif/' + searchTerm,
         opt = {
             method: 'POST'
         };
@@ -11,8 +19,11 @@ document.getElementById('search-btn').addEventListener('click', function(event){
         .then(function(resp){
             return resp.json();
         })
-        .then(function(json){
-            console.log(json);
+        .then(function(data){
+            document.getElementById('result').innerHTML = '';
+
+            renderImg(data.gifs);
+            renderImg(data.stickers);
         })
         .catch(function(err){
             console.log('An error has occurred: ' + err);
